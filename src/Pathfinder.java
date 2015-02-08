f84871a2f0f5944a0f58862bc8949c2271ef56ae
Sebastian Callh <sebastian.callh@gmail.com>
2015-02-08 11:09:54 +0100

    Initial push.

---
+++ src/Pathfinder.java
@@ -0,0 +1,15 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Queue;
+
+/**
+ * Created by Seba on 2015-02-06.
+ */
+public class Pathfinder {
+    /*public static Map<Tile.Type, Tile.Type> pathToOneFromAll(int[][] grid, int startX, int startY) {
+        Map<Tile.Type, Tile.Type> paths = new HashMap<Tile.Type, Tile.Type>();
+        Queue<Tile.Type> queue = new LinkedList<Tile.Type>();
+        queue.add(grid[startY][startX]);
+    }*/
+}
