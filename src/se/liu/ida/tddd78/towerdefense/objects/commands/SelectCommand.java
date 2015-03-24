aa62e4886ec987071d7964f5014d2081e425111a
SebastianCallh <sebastian.callh@gmail.com>
2015-03-24 22:12:41 +0100

    Support for selecting different tower types to construct using number keys

---
+++ src/se/liu/ida/tddd78/towerdefense/objects/commands/SelectCommand.java
@@ -0,0 +1,22 @@
+package se.liu.ida.tddd78.towerdefense.objects.commands;
+
+import se.liu.ida.tddd78.towerdefense.Board;
+import se.liu.ida.tddd78.towerdefense.Player;
+import se.liu.ida.tddd78.towerdefense.interfaces.Command;
+import se.liu.ida.tddd78.towerdefense.objects.defense.DefenseType;
+
+/**
+ * Created by Seba on 2015-03-24.
+ */
+public class SelectCommand implements Command {
+    DefenseType type;
+
+    public SelectCommand(DefenseType type) {
+        this.type = type;
+    }
+
+    @Override
+    public void execute(Player player, Board board) {
+        player.setSelectedDefense(this.type);
+    }
+}
