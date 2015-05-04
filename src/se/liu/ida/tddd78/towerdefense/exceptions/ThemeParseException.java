fc0f1d0e9f5ab85f8a39842b9a0d5b4c9897d5b0
Patrik Sletmo <patrik.sletmo@gmail.com>
2015-05-04 17:43:28 +0200

    Exception changes

--- src/se/liu/ida/tddd78/towerdefense/exceptions/ThemeParseException.java
+++ src/se/liu/ida/tddd78/towerdefense/exceptions/ThemeParseException.java
@@ -3,7 +3,7 @@ package se.liu.ida.tddd78.towerdefense.exceptions;
 /**
  * Exception thrown when the parsing of a theme fails for some reason, due to for example an incorrect tag or constant.
  */
-public class ThemeParseException extends ThemeLoadException {
+public class ThemeParseException extends Exception {
 
     public ThemeParseException(String message) {
         super(message);
